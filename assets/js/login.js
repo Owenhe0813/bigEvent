018215fd46b5636f9059f41755f8a8f6887cd235
Owenhe0813 <527376876@qq.com>
2020-06-06 22:10:17 +0800

    完成注册功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -22,8 +22,28 @@ $(function() {
                     return '密码不一致';
                 }
             }
-
         })
         //3.注册
+        // 3.1 表单验证： 1. 密码是否在6~12 位之间2.用户名是否为邮箱 -
+    $('.reg').on('submit', function(e) { //必须监听表单提交事件
+            //     3.1 提交数据 
+            e.preventDefault();
+            const data = $(this).serialize();
+            $.ajax({
+                type: 'POST',
+                url: 'http://www.liulongbin.top:3007/api/reguser',
+                data: data,
+                success: function(res) {
+
+                    // 3.2 弹出响应参数
+                    layer.msg(res.message);
+                    if (res.status === 0) {
+                        // 3.3 回跳到登录界面
+                        $('.toggleToLogin').trigger('click');
+                    }
+                }
+            })
+
+        })
         //4.登录
 });
