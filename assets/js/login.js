b772c809a843c6df44e1ae23e57b817b517e739b
Owenhe0813 <527376876@qq.com>
2020-06-06 20:48:43 +0800

    基础功能完成，模块间转跳以及表单验证的字符

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,14 +1,29 @@
 $(function() {
     // 1.切换
-    // 1.切换注册模块
+    // 1.1切换注册模块
     $('.toggleToReg').on('click', () => {
-
+        console.log(1);
+        $('.log').toggle();
+        $('.reg').toggle();
     });
     // 2.切换登录模块模块
-    $('.toggleToReg').on('click', () => {
-
+    $('.toggleToLogin').on('click', () => {
+        $('.log').toggle();
+        $('.reg').toggle();
     });
     //2.表单验证
-    //3.注册
-    //4.登录
+    const form = layui.form;
+    form.verify({
+            // 2.1 密码长度
+            len: [/^[\S]{6,12}$/, '输入密码不符合要求'],
+            // 2.2 验证的注册页两个密码是否一致
+            same: function(val) {
+                if ($('.checkit').val().trim() !== val) {
+                    return '密码不一致';
+                }
+            }
+
+        })
+        //3.注册
+        //4.登录
 });
